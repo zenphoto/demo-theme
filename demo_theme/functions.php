29fc4c18defb3df0075974de6016ecee043d763f
acrylian <wartgart@gmx.net>
2018-05-10 11:47:21 +0200

    Some small updates and comment adjustments

--- demo_theme/functions.php
+++ demo_theme/functions.php
@@ -1,7 +1,13 @@
 <?php
 /**
  * This file is loaded automatically if present and can contain extra functions to provide theme specific extra functionality.
- * It is recommend to prefix or suffix theme specific funtions so they are easily recognizable from standard Zenphoto functions
- * Examples : mytheme_printSomething() or printSomething_mytheme()
+ * You can prefix or suffix theme specific funtions so they are easily recognizable from standard Zenphoto functions
+ * But we actually recommend to use a (static) class to wrap them.
  */
-?>
+class myTheme {
+	
+	static function printSomething() {
+		echo "something";
+	}
+	
+}
